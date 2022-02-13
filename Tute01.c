cea09cfe3c9a861708ec825be82ad9f10ed1716a
IT21285042 <99561023+IT21285042@users.noreply.github.com>
2022-02-13 14:32:36 +0000

    I completed tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  int subject1, subject2, total;
+  float average;
+
+  printf("enter marks of subject 1 : ");
+  scanf("%d",&subject1);
+
+  printf("enter marks of subject 2 : ");
+  scanf("%d",&subject2);
+
+  total = subject1 + subject2;
+
+  average = total / 2;
+  printf("average is %.2f", average);
   
   return 0;
 }
--- Tute03.c
+++ Tute03.c
@@ -12,8 +12,21 @@ n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
-int main() {
-  
+int main()
+{
+
+  int number, sum = 0, i = 1;
+
+  printf("Enter number : ");    
+	scanf("%d", &number);
+
+  while (i <= number)
+  {
+    sum = sum + 1;
+    i++;
+  }
+
+  printf("Sum of the numbers from 1 to %d is %d", number, sum);
+
   return 0;
 }
-
